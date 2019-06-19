5cf629a3592d2e38bf689d257e86a11db3bab8b4
ch <595506193@qq.com>
2019-06-19 18:11:19 +0800

    Ristrat

---
+++ app/src/main/java/com/example/tidus/ristrat/mvp/model/CaseControlModel.java
@@ -0,0 +1,14 @@
+package com.example.tidus.ristrat.mvp.model;
+
+import com.example.tidus.ristrat.callback.IRequestCallback;
+import com.example.tidus.ristrat.contract.ICaseControlContract;
+
+import java.util.HashMap;
+
+public class CaseControlModel implements ICaseControlContract.ICaseControlModel {
+
+    @Override
+    public void getCaseControl(HashMap<String, Object> params, IRequestCallback iRequestCallback) {
+
+    }
+}
---
+++ app/src/main/java/com/example/tidus/ristrat/mvp/presenter/CaseControlPresenter.java
@@ -0,0 +1,12 @@
+package com.example.tidus.ristrat.mvp.presenter;
+
+import com.example.tidus.ristrat.contract.ICaseControlContract;
+
+import java.util.HashMap;
+
+public class CaseControlPresenter extends ICaseControlContract.CaseControlPresenter {
+    @Override
+    public void getCaseControl(HashMap<String, Object> params) {
+        
+    }
+}
